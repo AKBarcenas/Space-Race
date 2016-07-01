a9e27d8ace231a7130a05f275756612bb3f1a9ec
AKBarcenas <akbarcen@ucsd.edu>
2016-06-30 19:18:47 -0700

    Allow enemies to be created.

--- Space Race/GameScene.swift
+++ Space Race/GameScene.swift
@@ -7,17 +7,29 @@
 //
 
 import SpriteKit
+import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    // The starfield in the background.
     var starfield: SKEmitterNode!
+    // The sprite representing the user.
     var player: SKSpriteNode!
     
+    // Displays the user's score.
     var scoreLabel: SKLabelNode!
+    // Keeps track of the user's score.
     var score: Int = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
         }
     }
+    
+    // Names for all possible enemies.
+    var possibleEnemies = ["ball", "hammer", "tv"]
+    // A timer for the game.
+    var gameTimer: NSTimer!
+    // Keeps track of whether or not the game has ended.
+    var gameOver = false
     
     override func didMoveToView(view: SKView) {
         backgroundColor = UIColor.blackColor()
@@ -43,12 +55,55 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
+        
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
     }
    
+    /*
+     * Function Name: update
+     * Parameters: currentTime - the current system time.
+     * Purpose: This method updates the score as long as the game is not over and removes enemies that
+     *   are no longer needed.
+     * Return Value: None
+     */
+    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+        
+        if !gameOver {
+            score += 1
+        }
     }
+    
+    /*
+     * Function Name: createEnemy
+     * Parameters: None
+     * Purpose: This method randomly selects an enemy type and randomly places it on the right side
+     *   of the screen. The enemy is also given a horizontal velocity and angular velocity.
+     * Return Value: None
+     */
+    
+    func createEnemy() {
+        possibleEnemies = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(possibleEnemies) as! [String]
+        let randomDistribution = GKRandomDistribution(lowestValue: 50, highestValue: 736)
+        
+        let sprite = SKSpriteNode(imageNamed: possibleEnemies[0])
+        sprite.position = CGPoint(x: 1200, y: randomDistribution.nextInt())
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+    }
+    
 }
